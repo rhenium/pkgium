628a42e35ba45ecbaba923a46bed2f7f50f42a6f
Kazuki Yamaguchi <builder@rhe.jp>
2016-08-10 11:07:32 +0000

    chromium-edge: depot_tools の git 化

--- chromium-edge/build.sh
+++ chromium-edge/build.sh
@@ -5,122 +5,83 @@
 ###############################
 clang=1
 
-###############################
-### set environment ###########
-###############################
 _pwd=$(pwd)
-export PATH="$_pwd/python-path:$PATH"
-export PATH="$_pwd/depot_tools:$PATH"
+export PATH="$_pwd/python-path:$_pwd/depot_tools:$PATH"
 
 # Use Python 2.x
 if [ ! -f "$_pwd/python-path/python" ]; then
   mkdir python-path
   ln -s /usr/bin/python2 python-path/python
 fi
-  
+
 
 update() {
-  ###############################
-  ### fetch depot_tools
-  ###############################
+  cd "$_pwd"
+
   echo "preparing depot_tools...."
-  if [ -d depot_tools ]; then
-    svn update depot_tools
-  else
-    svn checkout https://src.chromium.org/svn/trunk/tools/depot_tools
-  fi
-  
-  ###############################
-  ### fetch source
-  ###############################
+  [ ! -d depot_tools ] && git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  git -C depot_tools pull origin master
+
   echo "obtaining source...."
-  if [ ! -d src ]; then
-    fetch blink --nosvn=true
-  fi
-  cd "$_pwd/src"
+  [ ! -d src ] && fetch blink --nosvn=true
+  cd src
   git pull origin master --no-edit || exit
   gclient sync --nohooks
-  
+
   ###############################
   ### configure chromium
   ###############################
   echo "configuring chromium...."
-  # Will be used in PKGBUILD
-  cd "$_pwd/depot_tools"
-  svn info | grep 'Revision' | awk '{ print $2; }' > "$_pwd/BUILD_VERSION"
-  
-  #export CXX="clang++ -Qunused-arguments -D__extern_always_inline=inline"
-  #if [ "$clang" = 1 ]; then
-  #  export CC="clang -Qunused-arguments"
-  #  export CXX="clang++ -Qunused-arguments"
-  #fi
-  
+
   export CFLAGS="-march=corei7-avx -mtune=corei7-avx -O2 -pipe"
   export CXXFLAGS="$CFLAGS"
-  
+
   export GYP_DEFINES="
     clang=$clang
     clang_use_chrome_plugins=0
-    make_clang_dir=/usr
     fastbuild=1
     werror=
     linux_sandbox_path=/usr/lib/chromium-edge/chrome-sandbox
-    linux_strip_binary=1
-    linux_use_bundled_binutils=0
-    linux_use_bundled_gold=0
-    linux_use_gold_flags=1
     usb_ids_path=/usr/share/hwdata/usb.ids
-    remove_webcore_debug_symbols=1
-    release_extra_cflags='-march=corei7-avx -mtune=corei7-avx -O2 -pipe'
-    logging_like_official_build=1
-  
+    release_extra_cflags='$CFLAGS'
+
     google_api_key='AIzaSyDwr302FpOSkGRpLlUpPThNTDPbXcIn_FM'
     google_default_client_id='413772536636.apps.googleusercontent.com'
     google_default_client_secret='0ZChLK6AxeA3Isu96MkwqDR4'
-  
+
     ffmpeg_branding=Chrome
     proprietary_codecs=1
-  
+
     enable_hidpi=0
     use_sysroot=0
     disable_nacl=1
   "
-  
+
   cd "$_pwd/src"
   gclient runhooks || exit
+  python build/gyp_chromium.py
 }
 
 build() {
-  ###############################
-  ### compile
-  ###############################
   echo "compiling...."
-  
+
   # WORKAROUND
   # Bundled LLVM seems to need libtinfo.so.5
-  # ln -sf /usr/lib/libncurses.so.5 "$_pwd/src/third_party/llvm-build/Release+Asserts/lib/libtinfo.so.5"
-  ## ann
-  # ln -sf /usr/lib/libstdc++.so.6 "$_pwd/src/third_party/llvm-build/Release+Asserts/lib/libstdc++.so.6"
-  
+  ln -sf /usr/lib/libncursesw.so.6 "$_pwd/src/third_party/llvm-build/Release+Asserts/lib/libtinfo.so.5"
+
   cd "$_pwd/src"
-  # ninja -j4 -C out/Release chrome chrome_sandbox chromedriver || exit
   ninja -j4 -C out/Release chrome chrome_sandbox || exit
 }
 
 package() {
-  ###############################
-  ### packaging
-  ###############################
   echo "making package...."
   cd "$_pwd/makepkg"
   BUILD_ROOT="$_pwd" makepkg -f || exit
-  
-  #sudo -uadmin cp chromium-edge-$(cat "$_pwd/BUILD_VERSION")-1-x86_64.pkg.tar.xz /nas/build/chromium-edge/
 }
 
 case "$1" in
-  update ) update ;;
-  build ) build ;;
-  package ) package;;
-  * ) update && build && package ;;
+  update) update ;;
+  build) build ;;
+  package) package ;;
+  *) update && build && package ;;
 esac
